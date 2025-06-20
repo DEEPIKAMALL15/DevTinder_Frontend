e4cd57feed56d1f18f32ecec297637740770c82f
DEEPIKAMALL15 <deepikam1508@gmail.com>
2025-06-20 21:17:34 +0530

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-export const BASE_URL = 
- location.hostname === "localhost" ? "http://localhost:3000":"https://devconnect-gx98.onrender.com" 
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000":"https://devconnect-gx98.onrender.com" 
 
